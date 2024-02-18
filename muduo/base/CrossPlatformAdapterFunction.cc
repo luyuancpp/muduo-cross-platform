cf321af074481f997b4091e53e395bb13489f6e9
luyuan <luyuan@qq.com>
2024-02-18 13:07:32 +0800

    build error

--- muduo/base/CrossPlatformAdapterFunction.cc
+++ muduo/base/CrossPlatformAdapterFunction.cc
@@ -304,31 +304,6 @@ int accept4(int sockfd, struct sockaddr *addr,
 #include <process.h>
 
 
-namespace muduo
-{
-    namespace ProcessInfo
-    {
-
-        std::string hostname()
-        {
-            // HOST_NAME_MAX 64
-            // _POSIX_HOST_NAME_MAX 255
-            char buf[256];
-            if (::gethostname(buf, sizeof buf) == 0)
-            {
-                buf[sizeof(buf) - 1] = '\0';
-                return buf;
-            }
-            else
-            {
-                return "unknownhost";
-            }
-        }
-
-    }
-
-}//namespace muduo
-
 
 #endif//WIN32
 
